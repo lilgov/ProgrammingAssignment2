bb4647c1fe9d7b57f3e1f1d1096c40ff30571b1a
Andrew Miller <ASM@Andrews-MacBook-Pro.local>
2017-01-01 14:39:58 -0800

    Added bodies of functions makeCacheMatrix and cacheSolve to enable solving for inverse of matrix, and caching the result so that subsequent calls for the inverse require no computation.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix  creates a list representing a matrix whose inverse
+##   is cached.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  xInverse <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    xInverse <<- NULL
+  }
+  
+  get <- function() x
+  setInverse <- function(inv) xInverse <<- inv
+  getInverse <- function() xInverse
+  
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
+
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setInverse(inv)
+  inv
 }
+
+
+
